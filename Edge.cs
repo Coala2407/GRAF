869a7f677fc9d394bac9154eb268256ab3c4411b
Coala2407 <36776480+Coala2407@users.noreply.github.com>
2020-03-04 10:22:36 +0100

    made classes and fields

---
+++ Edge.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GRAF
+{
+    class Edge
+    {
+        Node startNode;
+
+        Node edgeNode;
+    }
+}
---
+++ Graph.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GRAF
+{
+    class Graph
+    {
+        List<Node> nodeList = new List<Node>();
+        List<Edge> edgeList = new List<Edge>();
+    }
+}
